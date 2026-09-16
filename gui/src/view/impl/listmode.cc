reivelte/rin

commit bcee8f8c5118f2a1739f20b5a706c5b2cd841830

add horizontal scrollbar support to entity_view

gui/src/view/impl/listmode.cc
@@ -99,7 +99,9 @@ namespace rin
         void clear_node(const QModelIndex& index);
         void collapse_node(const QModelIndex& index);
         int content_height_for_node(const QModelIndex& index) const;
+        int content_width_for_node(const QModelIndex& index) const;
         void recalculate_content_height(); // used when icon size is changed
+        void recalculate_content_width();
         std::tuple<int, int> intersecting_range(const QRect& r) const;
         
         /* templates */
@@ -221,7 +223,7 @@ namespace rin
         {
             expand_node(root_index);
         }
-        
+
         if (!bsp.initialized())
         { create_bsp(); }
 
@@ -229,6 +231,8 @@ namespace rin
         {
             autoexpand_child_nodes(root_index);
         }
+
+        recalculate_content_width();
 
         const auto [start, end] = intersecting_range(bounds);
         return entity_view_layout_descriptor{
@@ -300,7 +304,7 @@ namespace rin
         const QRect prev_rect = opt.rect;
         
         opt.state &= ~QStyle::State_Selected;
-        opt.rect.setWidth(view->viewport()->rect().width());
+        opt.rect.setWidth(std::max(view->viewport()->width(), total_content_width));
         opt.rect.setLeft(0);
 
         painter.drawPrimitive(QStyle::PE_PanelItemViewRow, opt);
@@ -583,9 +587,37 @@ namespace rin
         return total_h;
     }
 
+    // assumes index occurs in tree
+    int entity_view::list_mode::content_width_for_node(const QModelIndex& index) const
+    {
+        int w = 0;
+        if (header)
+        {
+            const int c = model->columnCount(index);
+            for (int i = 0; i < c; ++i) // col count is expected to be the same for every node
+            {
+                if (i < c - 1)
+                { w += header->sectionSize(i); }
+                else
+                { w += view->sizeHintForColumn(i); } // last column is stretched, so get the size of its contents instead
+            }
+        }
+        else
+        {
+            // we assume active_column is the only visible column
+            w = view->sizeHintForColumn(active_column);
+        }
+        return w;
+    }
+
     void entity_view::list_mode::recalculate_content_height()
     {
         total_content_height = content_height_for_node(root_index);
+    }
+
+    void entity_view::list_mode::recalculate_content_width()
+    {
+        total_content_width = content_width_for_node(root_index);
     }
 
     std::tuple<int, int> entity_view::list_mode::intersecting_range(const QRect& r) const

gui/src/view/view.cpp
@@ -110,6 +110,7 @@ namespace rin
             auto* lv = m->as<list_mode*>();
             lv->clear_layout_data();
             lv->recalculate_content_height();
+            lv->recalculate_content_width();
         }
         else
         { m->clear(); }
@@ -360,13 +361,13 @@ namespace rin
         clearSelection();
         m->active_column = 0;
         m->root_index = index;
+        QAbstractItemView::setRootIndex(index); // will schedule a layout and updateGeometry()
         if (viewmode() == entity_view_mode::List)
         {
             auto* lv = m->as<list_mode*>();
             if (lv->header)
             { lv->header->setRootIndex(index); }
         }
-        QAbstractItemView::setRootIndex(index); // will schedule a layout and updateGeometry()
     }
 
     void entity_view::selectAll()
@@ -398,7 +399,7 @@ namespace rin
                 height = std::min(height, lv->header->maximumHeight());
                 setViewportMargins(0, height, 0, 0);
                 const QRect vg = viewport()->geometry();
-                const QRect hg(vg.left(), vg.top() - height, vg.width(), height);
+                const QRect hg(vg.left() - m->horizontal_offset(), vg.top() - height, std::max(m->total_content_width, vg.width()), height);
                 lv->header->setGeometry(hg);
                 QMetaObject::invokeMethod(lv->header, "updateGeometries");
             }
@@ -416,8 +417,7 @@ namespace rin
         }
         else
         {
-            m->content_size = QSize(viewport()->rect().size().width(), m->total_content_height);
-            m->update_vertical_scrollbar(QSize(m->item_max_width, m->item_max_height));
+            m->update_scrollbars(QSize(m->item_max_width, m->item_max_height));
         }
         m->block_geometry_updates = false;
         updateEditorGeometries();

gui/src/view/impl/core.cc
@@ -47,7 +47,6 @@ namespace rin
         QBasicTimer delayed_repaint_timer;
         QRect elasticband;
         QRect bounds;
-        QSize content_size;
         QSize icon_mode_icon_size;
         QSize list_mode_icon_size;
         QSize item_max_thumbnail_size;
@@ -60,6 +59,7 @@ namespace rin
         qreal item_aspect_ratio;
         int view_loaded_items; // an item is considered to be loaded if we have a size for it and a row to place it in
         int total_content_height;
+        int total_content_width;
         int layout_batch_size;
         int item_default_text_width;
         int item_single_line_text_height;
@@ -97,7 +97,7 @@ namespace rin
         
         /* may or may not be overridden */
         virtual void clear();
-        virtual void update_vertical_scrollbar(const QSize& step);
+        virtual void update_scrollbars(const QSize& step);
         virtual void scroll_contents_by(int dx, int dy, bool scroll_elastic_band);
         virtual int horizontal_offset() const;
         virtual int vertical_offset() const;
@@ -153,33 +153,44 @@ namespace rin
     {
         set_hover_index(QModelIndex());
         interrupt_delayed_item_layout();
-        content_size = QSize();
         view_loaded_items = 0;
         total_content_height = row_spacing_y;
+        total_content_width = 0;
         applied_layouts.clear();
         pending_layouts.clear();
         pending_layout_timer.stop();
     }
 
-    void entity_view::impl::update_vertical_scrollbar(const QSize& step) 
+    void entity_view::impl::update_scrollbars(const QSize& step) 
     {
         const bool both_scrollbars_auto = (view->verticalScrollBarPolicy() == Qt::ScrollBarAsNeeded) && (view->horizontalScrollBarPolicy() == Qt::ScrollBarAsNeeded);
-        QSize size_of_viewport = view->viewport()->size();
-        QSize size_of_contents = content_size;
+        const QSize size_of_viewport = view->viewport()->size();
 
-        bool vbar_will_show = size_of_contents.height() > size_of_viewport.height();
-        bool hbar_will_show = false;
+        bool vbar_will_show = total_content_height > size_of_viewport.height();
+        bool hbar_will_show = total_content_width > size_of_viewport.width();
 
-        view->verticalScrollBar()->setSingleStep(step.height() / 4);
-        view->verticalScrollBar()->setPageStep(size_of_viewport.height());
+        auto* vbar = view->verticalScrollBar();
+        auto* hbar = view->horizontalScrollBar();
+        vbar->setSingleStep(step.height() / 4);
+        vbar->setPageStep(size_of_viewport.height());
+        hbar->setSingleStep(step.width() / 4);
+        hbar->setPageStep(size_of_viewport.width());
 
         if (hbar_will_show)
-        { vbar_will_show = size_of_contents.height() > size_of_viewport.height() - view->horizontalScrollBar()->height(); }
+        { vbar_will_show = total_content_height > size_of_viewport.height() - hbar->height(); }
+
+        if (vbar_will_show)
+        { hbar_will_show = total_content_width > size_of_viewport.width() - vbar->width(); }
         
         if (both_scrollbars_auto && !vbar_will_show)
-        { view->verticalScrollBar()->setRange(0, 0); } // workaround QTBUG-39902 (see qlistview.cpp line 2037)
+        { vbar->setRange(0, 0); } // workaround QTBUG-39902 (see qlistview.cpp line 2037)
         else
-        { view->verticalScrollBar()->setRange(0, (size_of_contents.height() - size_of_viewport.height())); }
+        { vbar->setRange(0, (total_content_height - size_of_viewport.height())); }
+
+        if (both_scrollbars_auto && !hbar_will_show)
+        { hbar->setRange(0, 0); }
+        else
+        { hbar->setRange(0, total_content_width - size_of_viewport.width()); }
     }
 
     void entity_view::impl::scroll_contents_by(int dx, int dy, bool scroll_elastic_band)
@@ -393,7 +404,7 @@ namespace rin
     }
 
     inline int entity_view::impl::flip_x(int x) const
-    { return std::max(view->viewport()->width(), content_size.width()) - x; }
+    { return std::max(view->viewport()->width(), total_content_width) - x; }
 
     inline QPoint entity_view::impl::flip_x(const QPoint& p) const
     { return QPoint(flip_x(p.x()), p.y()); }

commit 8e620a79758807772d4ad665233cac51ac4fde07

fix view regressions caused by bcee8f8

gui/src/view/impl/listmode.cc
@@ -22,9 +22,16 @@ namespace rin
         QModelIndex parent{};
         node_state state = node_state::Collapsed;
         int depth{}; // depth level of the tree. used to derive the x_indent for items under this node
+        int largest_item_index{}; // item in the node with the largest content width, excluding thumbnail and indentation
         bool pending_expand = false;
 
         int size() const { return static_cast<int>(items.size()); }
+        int width(int x_indent_scale, int thumb_width) const // not including auxiliary columns
+        {
+            const int indent = depth * x_indent_scale + x_indent_scale;
+            return indent + thumb_width + items[largest_item_index].index_hint();
+        }
+        
     };
 
     struct expanded_node
@@ -62,6 +69,7 @@ namespace rin
         entity_view_header* header;
         int visible_items;
         int padding_y;
+        int padding_x;
         int x_indent_scale;
 
         list_mode(entity_view* parent);
@@ -102,6 +110,8 @@ namespace rin
         int content_width_for_node(const QModelIndex& index) const;
         void recalculate_content_height(); // used when icon size is changed
         void recalculate_content_width();
+        void recalculate_node_width(const QModelIndex& parent, bool update_widths);
+        QModelIndex widest_node() const;
         std::tuple<int, int> intersecting_range(const QRect& r) const;
         
         /* templates */
@@ -137,6 +147,7 @@ namespace rin
         inline int default_item_rect_width() const;
         inline int default_item_rect_height() const;
         inline QSize approximate_content_size(size_t item_count) const;
+        inline int content_width(const QModelIndex& index) const;
     };
 
     entity_view::list_mode::list_mode(entity_view* parent)
@@ -183,6 +194,7 @@ namespace rin
         active_column = 0;
 
         padding_y = 2;
+        padding_x = 8;
     }
 
     std::vector<std::tuple<QModelIndex, int>> entity_view::list_mode::intersecting_set(const QRect& r, bool do_layout)
@@ -232,8 +244,6 @@ namespace rin
         {
             autoexpand_child_nodes(root_index);
         }
-
-        recalculate_content_width();
 
         const auto [start, end] = intersecting_range(bounds);
         return entity_view_layout_descriptor{
@@ -469,6 +479,7 @@ namespace rin
             .parent = nodes.contains(ancestor) ? ancestor : root_index,
             .state = Collapsed,
             .depth = depth,
+            .largest_item_index = -1,
             .pending_expand = false
         });
 
@@ -485,15 +496,26 @@ namespace rin
         if (nodes[index].state == Expanded)
         { return; }
         
+        const QFontMetrics f = view->fontMetrics();
         const int count = model->rowCount(index);
         const int delta = count - static_cast<int>(nodes[index].items.size());
+        
         nodes[index].items.reserve(count);
+        
+        if (count && nodes[index].largest_item_index < 0)
+        { nodes[index].largest_item_index = 0;}
 
         for (int i = static_cast<int>(nodes[index].items.size()); i < count; ++i)
         {
             if (const QModelIndex index_for_item = model->index(i, active_column, index); index_for_item.isValid())
             {
-                nodes[index].items.append(i);
+                const QString text = model->data(index_for_item, Qt::DisplayRole).toString();
+                const int item_width = padding_x + f.horizontalAdvance(text);
+
+                nodes[index].items.append(item_width);
+
+                const int lii = nodes[index].largest_item_index;
+                nodes[index].largest_item_index = item_width > nodes[index].items[lii].index_hint() ? i : lii;
 
                 if (model->hasChildren(index_for_item))
                 { create_node(index_for_item, Collapsed, nodes[index].depth + 1); }
@@ -520,7 +542,8 @@ namespace rin
         { applied_global_positions.reserve(visible_items); }
 
         nodes[index].state = Expanded;
-
+        
+        recalculate_content_width();
         emit view->expanded(index);
     }
 
@@ -599,7 +622,14 @@ namespace rin
                 if (i < c - 1)
                 { w += header->sectionSize(i); }
                 else
-                { w += view->sizeHintForColumn(i); } // last column is stretched, so get the size of its contents instead
+                {
+                    const QModelIndex aux = model->index(index.row(), i, index.parent());
+                    if (aux.isValid() || (model_is_entity_model && entity_model_ptr()->valid_index(aux)))
+                    {
+                        const int cw = content_width(aux);
+                        w += (nodes[index].depth * x_indent_scale + x_indent_scale) + item_max_thumbnail_size.width() + padding_x + cw;
+                    }
+                }
             }
         }
         else
@@ -617,7 +647,65 @@ namespace rin
 
     void entity_view::list_mode::recalculate_content_width()
     {
-        total_content_width = content_width_for_node(root_index);
+        const QModelIndex parent = widest_node();
+        
+        if (parent == QModelIndex())
+        { return; }
+        
+        total_content_width = content_width_for_node(parent);
+    }
+
+    void entity_view::list_mode::recalculate_node_width(const QModelIndex& parent, bool update_widths)
+    {
+        const QFontMetrics f = view->fontMetrics();
+        auto get_item_content_width = [&](const QModelIndex& index) -> int
+        {
+            const QString text = model->data(index, Qt::DisplayRole).toString();
+            return padding_x + f.horizontalAdvance(text);
+        };
+        
+        auto& node = nodes[parent];
+        int lii = -1;
+        if (node.items.size())
+        {
+            ++lii;
+            if (update_widths)
+            {
+                for (int i = 0; std::cmp_less(i, node.items.size()); ++i)
+                {
+                    const QModelIndex index = model->index(i, active_column, parent);
+                    const int w = get_item_content_width(index);
+                    node.items[i].set_index_hint(w);
+                    lii = w > node.items[lii].index_hint() ? i : lii;
+                }
+            }
+            else
+            {
+                for (int i = 0; std::cmp_less(i, node.items.size()); ++i)
+                {
+                    const int w = node.items[i].index_hint();
+                    lii = w > node.items[lii].index_hint() ? i : lii;
+                }
+            }
+        }
+        node.largest_item_index = lii;
+    }
+
+    QModelIndex entity_view::list_mode::widest_node() const
+    {
+        QModelIndex index;
+        int w = 0;
+        traverse_tree([&](const QModelIndex& parent) -> bool
+        {
+            const int lii = nodes[parent].largest_item_index;
+            if (const int nw = nodes[parent].items[lii].index_hint(); nw > w)
+            {
+                w = nw;
+                index = parent;
+            }
+            return false;
+        });
+        return index;
     }
 
     std::tuple<int, int> entity_view::list_mode::intersecting_range(const QRect& r) const
@@ -976,6 +1064,13 @@ namespace rin
     inline QSize entity_view::list_mode::approximate_content_size(size_t item_count) const
     {
         return QSize(view->viewport()->rect().width(), default_item_rect_height() * static_cast<int>(item_count));
+    }
+
+    // does not include padding
+    inline int entity_view::list_mode::content_width(const QModelIndex& index) const
+    {
+        const QFontMetrics f = view->fontMetrics();
+        return f.horizontalAdvance(model->data(index, Qt::DisplayRole).toString());
     }
 
 } // namespace rin

gui/src/view/view.cpp
@@ -333,20 +333,30 @@ namespace rin
         if (viewmode() == entity_view_mode::List)
         {
             auto* lv = m->as<list_mode*>();
-            const auto f = fontMetrics();
             const int thumb_width = m->item_max_thumbnail_size.width();
-            lv->traverse_tree([&](const QModelIndex& parent, int gstart, int lstart, int range) -> bool
+
+            if (column == lv->active_column)
             {
-                Q_UNUSED(gstart);
-                for (int i = lstart; i <= (lstart + range); ++i)
+                lv->traverse_tree([&](const QModelIndex& parent) -> bool
                 {
-                    const QModelIndex index = m->model->index(i, column, parent);
-                    const QString text = m->model->data(index, Qt::DisplayRole).toString();
-                    const int indent = column == 0 ? lv->nodes[parent].depth * lv->x_indent_scale + lv->x_indent_scale : 0;
-                    width = std::max(width, indent + thumb_width + 8 + f.horizontalAdvance(text)); // TODO: remove hardcoded values
-                }
-                return false;
-            });
+                    width = std::max(width, lv->nodes[parent].width(lv->x_indent_scale, thumb_width));
+                    return false;
+                });
+            }
+            else
+            {
+                const QFontMetrics f = fontMetrics();
+                lv->traverse_tree([&](const QModelIndex& parent, int gstart, int lstart, int range) -> bool
+                {
+                    Q_UNUSED(gstart);
+                    for (int i = lstart; i <= (lstart + range); ++i)
+                    {
+                        const int cw = f.horizontalAdvance(lv->model->data(m->model->index(i, column, parent), Qt::DisplayRole).toString());
+                        width = std::max(width, lv->padding_x + cw);
+                    }
+                    return false;
+                });
+            }
         }
         return width;
     }
@@ -1133,6 +1143,13 @@ namespace rin
                 const int amount_removed = (end - start) + 1;
                 m->view_loaded_items -= amount_removed;
                 m->total_content_height -= amount_removed * lv->default_item_rect_height();
+
+                if (const int lii = lv->nodes[parent].largest_item_index; (start <= lii) && (lii <= end))
+                {
+                    lv->recalculate_node_width(parent, false);
+                    lv->recalculate_content_width();
+                }
+
                 lv->clear_layout_data();
             }
         }
@@ -1206,6 +1223,7 @@ namespace rin
         {
             auto* lv = m->as<list_mode*>();
             lv->clear_layout_data();
+            lv->recalculate_content_width();
             scheduleDelayedItemsLayout();
         }
     }